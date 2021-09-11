ac10148d2d4bd3e52e5e15b155a4f2ef3f86dae7
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-09-11 14:28:22 -0400

    added requires to server.test, requiring db, supertest, and the server

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,9 @@
 // Write your tests here
+
+const db = require("../data/dbConfig.js")
+const request = require("supertest")
+const server = require("./server")
+
 test('sanity', () => {
   expect(true).toBe(true)
 })
